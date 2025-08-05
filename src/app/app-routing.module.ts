ae9feafeb1d68959d75b4892bdbe27ac796cab15
Malik <Malik.rabea17@gmail.com>
2025-08-05 11:40:44 -0700

    End

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ShopComponent } from './shop/shop.component';
+import { HomeComponent } from './home/home.component';
+import { ProductDetailsComponent } from './shop/product-details/product-details.component';
+import { authGuard } from './guard/auth.guard';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  {
+    path: 'shop',
+    loadChildren: () => import('./shop/shop.module').then((m) => m.ShopModule), 
+    
+  },
+  {
+    path: 'basket',
+    loadChildren: () => import('./basket/basket.module').then((m) => m.BasketModule), 
+    
+  },
+  {
+    path: 'checkout',
+    loadChildren: () => import('./checkout/checkout.module').then((m) => m.CheckoutModule), 
+    canActivate:[authGuard]
+  },
+  {
+    path: 'Account',
+    loadChildren: () => import('./identity/identity.module').then((m) => m.IdentityModule), 
+    
+  },
+  {
+    path: 'orders',
+    loadChildren: () => import('./orders/orders.module').then((m) => m.OrdersModule), 
+    canActivate:[authGuard]
+  },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
